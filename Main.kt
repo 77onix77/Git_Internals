8ec208699f69ea4a99d208f2c45aa5b021352bc7
77onix77 <77onix77@gmail.com>
2022-09-13 20:22:55 +0300

    stage 2

---
+++ Main.kt
@@ -0,0 +1,22 @@
+package gitinternals
+
+import java.io.File
+import java.util.zip.Inflater
+
+fun main() {
+    println("Enter .git directory location:")
+    val path = readln()
+    println("Enter git object hash:")
+    val hash = readln()
+    val fullPath = "$path\\objects\\${hash.substring(0, 2)}\\${hash.substring(2)}"
+    val file = File(fullPath)
+    val arrayByte = file.readBytes()
+    val decompressor = Inflater()
+    decompressor.setInput(arrayByte)
+    val res = ByteArray(1000)
+    val str = decompressor.inflate(res)
+    decompressor.end()
+    val content = String(res,0, str).replace(Char(0), '\n')
+    val firstString = content.substringBefore('\n').split(" ")
+    println("type:${firstString[0]} length:${firstString[1]}")
+}
