af7cd006cd6a62ffe3281cd9f4c5a1e942ba5157
77onix77 <77onix77@gmail.com>
2022-09-14 21:32:36 +0300

    import kotlinx

--- Main.kt
+++ Main.kt
@@ -2,6 +2,7 @@ package gitinternals
 
 import java.io.File
 import java.util.zip.Inflater
+import kotlinx.datetime.*
 
 fun main() {
     println("Enter .git directory location:")
@@ -19,4 +20,6 @@ fun main() {
     val content = String(res,0, str).replace(Char(0), '\n')
     val firstString = content.substringBefore('\n').split(" ")
     println("type:${firstString[0]} length:${firstString[1]}")
+
+
 }
