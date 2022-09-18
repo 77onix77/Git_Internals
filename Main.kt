7e581c70fddd5964d6a67fb834a29e8cb3c14fb0
77onix77 <77onix77@gmail.com>
2022-09-18 15:55:06 +0300

    Stage 3

--- Main.kt
+++ Main.kt
@@ -1,8 +1,65 @@
 package gitinternals
 
 import java.io.File
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.zip.Inflater
-import kotlinx.datetime.*
+
+
+fun blob(listStr: List<String>) {
+    println("*BLOB*")
+    for (i in 1..listStr.lastIndex) println(listStr[i])
+}
+
+fun commit(listStr: MutableList<String>) {
+    println("*COMMIT*")
+    var par = 0
+    var par1 = 0
+    val listInd = mutableListOf<Int>()
+    for (i in 0..listStr.lastIndex) {
+        if (listStr[i].contains("parent ")) {
+            par++
+            if (par == 1) {
+                listStr[i] = listStr[i].replace("parent", "parents")
+                par1 = i
+            } else {
+                listStr[par1] += " | ${listStr[i].substringAfter(" ")}"
+                listInd += i
+            }
+        }
+    }
+    for (el in listInd) listStr.removeAt(el)
+    for (i in 1..listStr.lastIndex - 1) {
+        if (listStr[i].isEmpty()) {
+            println("commit message:")
+            continue
+        }
+        var str = listStr[i]
+        str = str.replace("tree", "tree:")
+        str = str.replace("parents", "parents:")
+        str = str.replace("<", "")
+        str = str.replace(">", "")
+        str = str.replace("author", "author:")
+        str = str.replace("committer", "committer:")
+        if (str.contains(Regex("\\d{10} [-|+]\\d{4}"))) {
+            str = str.replace(Regex("\\d{10} [-|+]\\d{4}"), timeMod(str))
+        }
+        println(str)
+    }
+}
+
+fun timeMod(str: String): String {
+    val list = str.split(" ")
+    val t = Instant.ofEpochSecond(list[list.lastIndex - 1].toLong()).atZone(ZoneOffset.of(list[list.lastIndex])).format(
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss xxx"))
+    var time = t.toString()
+    time = time.replace("T", " ")
+    time = time.replace("Z", "")
+    val res = if (list[0] == "author:") "original timestamp: $time"
+    else "commit timestamp: $time"
+    return res
+}
 
 fun main() {
     println("Enter .git directory location:")
@@ -18,8 +75,9 @@ fun main() {
     val str = decompressor.inflate(res)
     decompressor.end()
     val content = String(res,0, str).replace(Char(0), '\n')
-    val firstString = content.substringBefore('\n').split(" ")
-    println("type:${firstString[0]} length:${firstString[1]}")
-
+    val listStr = content.split('\n').toMutableList()
+    if (listStr[0].contains("blob")) blob(listStr)
+    if (listStr[0].contains("commit")) commit(listStr)
+    
 
 }
